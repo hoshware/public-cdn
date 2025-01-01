a095946e1f16f52e4eebdffe32a5c4d2cf29aedf
Hoang Nguyen <hnguyen.email@gmail.com>
2025-01-01 15:44:10 -0800

    Add initial files, conversion script, geocode script, prettier, env example

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
-# public-cdn
-For use with cdn services
+# Public CDN Repository
+
+This repository contains files intended for use with websites created by me. These files are provided for personal or educational use only and may **not** be used for commercial purposes.
+
+## License
+
+This project is licensed under the [Creative Commons Attribution-NonCommercial 4.0 International License](https://creativecommons.org/licenses/by-nc/4.0/). You are free to:
+
+- Share: Copy and redistribute the material in any medium or format.
+- Adapt: Remix, transform, and build upon the material.
+
+**Under the following terms:**
+
+- **Attribution**: You must give appropriate credit, provide a link to the license, and indicate if changes were made.
+- **NonCommercial**: You may not use the material for commercial purposes.
+
+For any inquiries or requests for commercial use, please contact [your email or contact information].
+
+![License: CC BY-NC 4.0](https://img.shields.io/badge/License-CC%20BY--NC%204.0-lightgrey.svg)
---
+++ convert-to-webp.esm.js
@@ -0,0 +1,53 @@
+import fs from 'fs/promises';
+import path from 'path';
+import sharp from 'sharp';
+
+// Define the directory containing images
+const __dirname = path.dirname(new URL(import.meta.url).pathname);
+
+// Function to convert PNG to WebP
+const convertToWebP = async (filePath) => {
+  const fileName = path.basename(filePath, path.extname(filePath)); // Get the file name without extension
+  const outputFilePath = path.join(path.dirname(filePath), `${fileName}.webp`);
+
+  try {
+    // Convert the image to WebP
+    await sharp(filePath)
+      .webp({
+        quality: 45,
+      })
+      .toFile(outputFilePath);
+    console.log(`Converted: ${filePath} → ${outputFilePath}`);
+  } catch (error) {
+    console.error(`Failed to convert ${filePath}:`, error);
+  }
+};
+
+// Function to process all PNG files in the directory
+const processImages = async (folderPath) => {
+  const imagesDir = path.resolve(__dirname, folderPath);
+  try {
+    const files = await fs.readdir(imagesDir);
+
+    // Filter jpg and png files and convert them
+    const jpgFiles = files.filter((file) =>
+      ['.jpg', '.jpeg', '.png'].includes(path.extname(file).toLowerCase()),
+    );
+    if (jpgFiles.length === 0) {
+      console.log('No PNG files found in the directory.');
+      return;
+    }
+
+    for (const file of jpgFiles) {
+      const filePath = path.join(imagesDir, file);
+      await convertToWebP(filePath);
+    }
+
+    console.log('All files have been processed.');
+  } catch (error) {
+    console.error('Error processing images:', error);
+  }
+};
+
+// Run the script
+processImages('dogparkly.com/images');
